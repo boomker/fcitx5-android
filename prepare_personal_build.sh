a9567125b559133f6b66633e527808bcd5a6885d
Shingo <boomker@users.noreply.github.com>
2026-03-18 22:25:40 +0800

    ci: use boomker prebuilt sources

--- prepare_personal_build.sh
+++ prepare_personal_build.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/bash
 
+FCITX5_RIME_REPO="${FCITX5_RIME_REPO:-https://github.com/fxliang/fcitx5-rime.git}"
+PREBUILT_REPO="${PREBUILT_REPO:-https://github.com/boomker/prebuilt.git}"
+PREBUILDER_REPO="${PREBUILDER_REPO:-https://github.com/boomker/prebuilder.git}"
+
 # update fcitx5-rime
-echo "updating fcitx5-rime"
+echo "updating fcitx5-rime from ${FCITX5_RIME_REPO}"
 pushd plugin/rime/src/main/cpp/fcitx5-rime
-git remote add gh https://github.com/fxliang/fcitx5-rime.git || git remote set-url gh https://github.com/fxliang/fcitx5-rime.git
+git remote add gh "${FCITX5_RIME_REPO}" || git remote set-url gh "${FCITX5_RIME_REPO}"
 git fetch -v gh master
 git checkout gh/master
 popd
 sed -i 's|/fcitx/|/fxliang/|g' plugin/rime/licenses/libraries/fcitx5-rime.json
 
 # update prebuilt
-echo "updating prebuilt"
+echo "updating prebuilt from ${PREBUILT_REPO}"
+echo "prebuilt producer repo is ${PREBUILDER_REPO}"
 pushd lib/fcitx5/src/main/cpp/prebuilt
-git remote add gh https://github.com/fxliang/prebuilt.git || git remote set-url gh https://github.com/fxliang/prebuilt.git
+git remote add gh "${PREBUILT_REPO}" || git remote set-url gh "${PREBUILT_REPO}"
 git fetch -v gh master
 git checkout gh/master
 popd
