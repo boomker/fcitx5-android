233f07505e73755d79b4181420d476302a341fa1
fxliang <fx.liang@outlook.com>
2026-04-02 12:33:32 +0800

    fix: sync long-press action type between kawaii and keyboard

--- app/src/main/java/org/fcitx/fcitx5/android/input/action/ButtonAction.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/input/action/ButtonAction.kt
@@ -19,11 +19,11 @@ import org.fcitx.fcitx5.android.data.prefs.AppPrefs
 import org.fcitx.fcitx5.android.input.FcitxInputMethodService
 import org.fcitx.fcitx5.android.input.clipboard.ClipboardWindow
 import org.fcitx.fcitx5.android.input.dialog.AddMoreInputMethodsPrompt
+import org.fcitx.fcitx5.android.input.dialog.InputMethodPickerDialog
 import org.fcitx.fcitx5.android.input.editing.TextEditingWindow
 import org.fcitx.fcitx5.android.input.keyboard.LangSwitchBehavior
 import org.fcitx.fcitx5.android.input.wm.InputWindowManager
 import org.fcitx.fcitx5.android.utils.AppUtil
-import org.fcitx.fcitx5.android.utils.InputMethodUtil
 import org.fcitx.fcitx5.android.utils.switchToNextIME
 
 /**
@@ -315,7 +315,11 @@ data object LanguageSwitchAction : ButtonAction() {
         windowManager: InputWindowManager,
         view: View
     ) {
-        InputMethodUtil.showPicker()
+        fcitx.launchOnReady {
+            service.lifecycleScope.launch {
+                service.showDialog(InputMethodPickerDialog.build(it, service, context))
+            }
+        }
     }
 }
 
