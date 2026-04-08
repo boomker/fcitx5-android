46dae442b5173198c2f1c5663844858078721778
Shingo <boomker@users.noreply.github.com>
2026-04-08 09:39:33 +0800

    omx(team): auto-checkpoint worker-2 [unknown]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,7 @@ android {
         debug {
             resValue("mipmap", "app_icon", "@mipmap/ic_launcher_debug")
             resValue("mipmap", "app_icon_round", "@mipmap/ic_launcher_round_debug")
+            resValue("string", "app_name", "帝鹅输入法")
         }
     }
 
