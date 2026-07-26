iovxw/fcitx5-android-poc

Manage edit/delete button with builtin Lifecycle

app/src/main/java/org/fcitx/fcitx5/android/ui/common/DynamicListAdapter.kt
@@ -15,6 +15,7 @@ import androidx.annotation.CallSuper
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import org.fcitx.fcitx5.android.ui.main.MainViewModel
+import org.fcitx.fcitx5.android.ui.main.MainViewModel.ButtonMode
 import java.util.Collections
 
 abstract class DynamicListAdapter<T>(
@@ -161,18 +162,14 @@ abstract class DynamicListAdapter<T>(
                 }
             }
             onBackPressedDispatcher.addCallback(onBackPressedCallback!!)
-            it.enableToolbarDeleteButton {
-                deleteSelected()
-                exitMultiSelect()
-            }
-            it.hideToolbarEditButton()
+            it.toolbarButton.value = ButtonMode.DELETE
             multiselect = true
             notifyDataSetChanged()
         }
     }
 
 
-    private fun deleteSelected() {
+    internal fun deleteSelected() {
         if (!multiselect || selected.isEmpty())
             return
         val indexed = selected.mapNotNull { entry ->
@@ -192,12 +189,10 @@ abstract class DynamicListAdapter<T>(
             if (!multiselect)
                 return
             onBackPressedCallback?.remove()
-            it.disableToolbarDeleteButton()
             multiselect = false
             selected.clear()
             notifyDataSetChanged()
-            if (entries.isNotEmpty())
-                it.showToolbarEditButton()
+            it.toolbarButton.value = if (entries.isNotEmpty()) ButtonMode.EDIT else ButtonMode.NONE
         }
     }
 
@@ -208,7 +203,7 @@ abstract class DynamicListAdapter<T>(
         _entries.add(idx, item)
         notifyItemInserted(idx)
         listener?.onItemAdded(idx, item)
-        mainViewModel?.showToolbarEditButton()
+        mainViewModel?.toolbarButton?.value = ButtonMode.EDIT
     }
 
     @CallSuper
@@ -217,7 +212,7 @@ abstract class DynamicListAdapter<T>(
         notifyItemRemoved(idx)
         listener?.onItemRemoved(idx, item)
         if (entries.isEmpty())
-            mainViewModel?.hideToolbarEditButton()
+            mainViewModel?.toolbarButton?.value = ButtonMode.NONE
         return item
     }
 

app/src/main/java/org/fcitx/fcitx5/android/ui/main/settings/ListFragment.kt
@@ -11,16 +11,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
 import org.fcitx.fcitx5.android.core.Key
 import org.fcitx.fcitx5.android.core.RawConfig
 import org.fcitx.fcitx5.android.ui.common.BaseDynamicListUi
 import org.fcitx.fcitx5.android.ui.common.DynamicListUi
 import org.fcitx.fcitx5.android.ui.main.EditDeleteMenuProvider
 import org.fcitx.fcitx5.android.ui.main.MainViewModel
+import org.fcitx.fcitx5.android.ui.main.MainViewModel.ButtonMode
 import org.fcitx.fcitx5.android.utils.config.ConfigDescriptor
 import org.fcitx.fcitx5.android.utils.config.ConfigType
 import org.fcitx.fcitx5.android.utils.lazyRoute
-import splitties.resources.styledColor
 
 class ListFragment : Fragment() {
 
@@ -139,24 +140,27 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.toolbarButton.value =
+            if (ui.entries.isNotEmpty()) ButtonMode.EDIT else ButtonMode.NONE
         requireActivity().addMenuProvider(
-            EditDeleteMenuProvider(viewModel, requireActivity(), viewLifecycleOwner),
-            viewLifecycleOwner
+            EditDeleteMenuProvider(
+                buttonMode = viewModel.toolbarButton,
+                editButtonAction = { ui.enterMultiSelect(requireActivity().onBackPressedDispatcher) },
+                deleteButtonAction = { ui.deleteSelected(); ui.exitMultiSelect() },
+                menuHost = requireActivity(),
+                lifecycleOwner = viewLifecycleOwner,
+            ),
+            viewLifecycleOwner,
+            Lifecycle.State.STARTED
         )
     }
 
     override fun onStart() {
         super.onStart()
         viewModel.setToolbarTitle(descriptor.description ?: descriptor.name)
-        if (uiInitialized) {
-            viewModel.enableToolbarEditButton(ui.entries.isNotEmpty()) {
-                ui.enterMultiSelect(requireActivity().onBackPressedDispatcher)
-            }
-        }
     }
 
     override fun onStop() {
-        viewModel.disableToolbarEditButton()
         if (uiInitialized) {
             ui.exitMultiSelect()
         }

app/src/main/java/org/fcitx/fcitx5/android/ui/main/settings/PunctuationEditorFragment.kt
@@ -6,6 +6,7 @@ package org.fcitx.fcitx5.android.ui.main.settings
 
 import android.app.AlertDialog
 import android.view.View
+import androidx.lifecycle.Lifecycle
 import org.fcitx.fcitx5.android.R
 import org.fcitx.fcitx5.android.core.RawConfig
 import org.fcitx.fcitx5.android.core.getPunctuationConfig
@@ -15,12 +16,12 @@ import org.fcitx.fcitx5.android.data.punctuation.PunctuationMapEntry
 import org.fcitx.fcitx5.android.ui.common.BaseDynamicListUi
 import org.fcitx.fcitx5.android.ui.common.OnItemChangedListener
 import org.fcitx.fcitx5.android.ui.main.EditDeleteMenuProvider
+import org.fcitx.fcitx5.android.ui.main.MainViewModel.ButtonMode
 import org.fcitx.fcitx5.android.utils.NaiveDustman
 import org.fcitx.fcitx5.android.utils.lazyRoute
 import org.fcitx.fcitx5.android.utils.materialTextInput
 import org.fcitx.fcitx5.android.utils.onPositiveButtonClick
 import org.fcitx.fcitx5.android.utils.str
-import splitties.resources.styledColor
 import splitties.views.dsl.core.add
 import splitties.views.dsl.core.lParams
 import splitties.views.dsl.core.matchParent
@@ -140,13 +141,19 @@ class PunctuationEditorFragment : ProgressFragment(), OnItemChangedListener<Punc
             }
         }
         resetDustman()
+        viewModel.toolbarButton.value =
+            if (ui.entries.isNotEmpty()) ButtonMode.EDIT else ButtonMode.NONE
         requireActivity().addMenuProvider(
-            EditDeleteMenuProvider(viewModel, requireActivity(), viewLifecycleOwner),
-            viewLifecycleOwner
+            EditDeleteMenuProvider(
+                buttonMode = viewModel.toolbarButton,
+                editButtonAction = { ui.enterMultiSelect(requireActivity().onBackPressedDispatcher) },
+                deleteButtonAction = { ui.deleteSelected(); ui.exitMultiSelect() },
+                menuHost = requireActivity(),
+                lifecycleOwner = viewLifecycleOwner,
+            ),
+            viewLifecycleOwner,
+            Lifecycle.State.STARTED
         )
-        viewModel.enableToolbarEditButton(initialEntries.isNotEmpty()) {
-            ui.enterMultiSelect(requireActivity().onBackPressedDispatcher)
-        }
         return ui.root
     }
 
@@ -173,16 +180,10 @@ class PunctuationEditorFragment : ProgressFragment(), OnItemChangedListener<Punc
     override fun onStart() {
         super.onStart()
         viewModel.setToolbarTitle(args.title)
-        if (isInitialized) {
-            viewModel.enableToolbarEditButton(ui.entries.isNotEmpty()) {
-                ui.enterMultiSelect(requireActivity().onBackPressedDispatcher)
-            }
-        }
     }
 
     override fun onStop() {
         saveConfig()
-        viewModel.disableToolbarEditButton()
         if (isInitialized) {
             ui.exitMultiSelect()
         }

app/src/main/java/org/fcitx/fcitx5/android/ui/main/settings/QuickPhraseListFragment.kt
@@ -19,6 +19,7 @@ import androidx.appcompat.widget.PopupMenu
 import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -33,6 +34,7 @@ import org.fcitx.fcitx5.android.ui.common.BaseDynamicListUi
 import org.fcitx.fcitx5.android.ui.common.OnItemChangedListener
 import org.fcitx.fcitx5.android.ui.main.EditDeleteMenuProvider
 import org.fcitx.fcitx5.android.ui.main.MainViewModel
+import org.fcitx.fcitx5.android.ui.main.MainViewModel.ButtonMode
 import org.fcitx.fcitx5.android.utils.NaiveDustman
 import org.fcitx.fcitx5.android.utils.importErrorDialog
 import org.fcitx.fcitx5.android.utils.item
@@ -300,9 +302,18 @@ class QuickPhraseListFragment : Fragment(), OnItemChangedListener<QuickPhrase> {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.toolbarButton.value =
+            if (ui.entries.isNotEmpty()) ButtonMode.EDIT else ButtonMode.NONE
         requireActivity().addMenuProvider(
-            EditDeleteMenuProvider(viewModel, requireActivity(), viewLifecycleOwner),
-            viewLifecycleOwner
+            EditDeleteMenuProvider(
+                buttonMode = viewModel.toolbarButton,
+                editButtonAction = { ui.enterMultiSelect(requireActivity().onBackPressedDispatcher) },
+                deleteButtonAction = { ui.deleteSelected(); ui.exitMultiSelect() },
+                menuHost = requireActivity(),
+                lifecycleOwner = viewLifecycleOwner,
+            ),
+            viewLifecycleOwner,
+            Lifecycle.State.STARTED
         )
     }
 
@@ -323,18 +334,8 @@ class QuickPhraseListFragment : Fragment(), OnItemChangedListener<QuickPhrase> {
         dustman.addOrUpdate(new.name, new.isEnabled)
     }
 
-    override fun onStart() {
-        super.onStart()
-        if (uiInitialized) {
-            viewModel.enableToolbarEditButton(ui.entries.isNotEmpty()) {
-                ui.enterMultiSelect(requireActivity().onBackPressedDispatcher)
-            }
-        }
-    }
-
     override fun onStop() {
         reloadQuickPhrase()
-        viewModel.disableToolbarEditButton()
         if (uiInitialized) {
             ui.exitMultiSelect()
         }

app/src/main/java/org/fcitx/fcitx5/android/ui/main/settings/im/InputMethodListFragment.kt
@@ -6,6 +6,7 @@ package org.fcitx.fcitx5.android.ui.main.settings.im
 
 import android.os.Build
 import android.view.View
+import androidx.lifecycle.Lifecycle
 import org.fcitx.fcitx5.android.core.InputMethodEntry
 import org.fcitx.fcitx5.android.core.SubtypeManager
 import org.fcitx.fcitx5.android.daemon.launchOnReady
@@ -13,10 +14,10 @@ import org.fcitx.fcitx5.android.ui.common.BaseDynamicListUi
 import org.fcitx.fcitx5.android.ui.common.DynamicListUi
 import org.fcitx.fcitx5.android.ui.common.OnItemChangedListener
 import org.fcitx.fcitx5.android.ui.main.EditDeleteMenuProvider
+import org.fcitx.fcitx5.android.ui.main.MainViewModel.ButtonMode
 import org.fcitx.fcitx5.android.ui.main.settings.ProgressFragment
 import org.fcitx.fcitx5.android.ui.main.settings.SettingsRoute
 import org.fcitx.fcitx5.android.utils.navigateWithAnim
-import splitties.resources.styledColor
 
 class InputMethodListFragment : ProgressFragment(), OnItemChangedListener<InputMethodEntry> {
 
@@ -53,30 +54,26 @@ class InputMethodListFragment : ProgressFragment(), OnItemChangedListener<InputM
         )
         ui.addOnItemChangedListener(this@InputMethodListFragment)
         ui.setViewModel(viewModel)
+        viewModel.toolbarButton.value =
+            if (ui.entries.isNotEmpty()) ButtonMode.EDIT else ButtonMode.NONE
         requireActivity().addMenuProvider(
-            EditDeleteMenuProvider(viewModel, requireActivity(), viewLifecycleOwner),
-            viewLifecycleOwner
+            EditDeleteMenuProvider(
+                buttonMode = viewModel.toolbarButton,
+                editButtonAction = { ui.enterMultiSelect(requireActivity().onBackPressedDispatcher) },
+                deleteButtonAction = { ui.deleteSelected(); ui.exitMultiSelect() },
+                menuHost = requireActivity(),
+                lifecycleOwner = viewLifecycleOwner,
+            ),
+            viewLifecycleOwner,
+            Lifecycle.State.STARTED
         )
-        viewModel.enableToolbarEditButton(initialEnabled.isNotEmpty()) {
-            ui.enterMultiSelect(requireActivity().onBackPressedDispatcher)
-        }
         return ui.root
-    }
-
-    override fun onStart() {
-        super.onStart()
-        if (::ui.isInitialized) {
-            viewModel.enableToolbarEditButton(ui.entries.isNotEmpty()) {
-                ui.enterMultiSelect(requireActivity().onBackPressedDispatcher)
-            }
-        }
     }
 
     override fun onStop() {
         if (::ui.isInitialized) {
             ui.exitMultiSelect()
         }
-        viewModel.disableToolbarEditButton()
         super.onStop()
     }
 

boomker/fcitx5-android

app/src/main/java/org/fxboomk/fcitx5/android/ui/common/DynamicListAdapter.kt
@@ -15,6 +15,7 @@ import androidx.annotation.CallSuper
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import org.fxboomk.fcitx5.android.ui.main.MainViewModel
+import org.fxboomk.fcitx5.android.ui.main.MainViewModel.ButtonMode
 import java.util.Collections
 
 abstract class DynamicListAdapter<T>(
@@ -163,18 +164,14 @@ abstract class DynamicListAdapter<T>(
                 }
             }
             onBackPressedDispatcher.addCallback(onBackPressedCallback!!)
-            it.enableToolbarDeleteButton {
-                deleteSelected()
-                exitMultiSelect()
-            }
-            it.hideToolbarEditButton()
+            it.toolbarButton.value = ButtonMode.DELETE
             multiselect = true
             notifyDataSetChanged()
         }
     }
 
 
-    private fun deleteSelected() {
+    internal fun deleteSelected() {
         if (!multiselect || selected.isEmpty())
             return
         val indexed = selected.mapNotNull { entry ->
@@ -194,12 +191,10 @@ abstract class DynamicListAdapter<T>(
             if (!multiselect)
                 return
             onBackPressedCallback?.remove()
-            it.disableToolbarDeleteButton()
             multiselect = false
             selected.clear()
             notifyDataSetChanged()
-            if (entries.isNotEmpty())
-                it.showToolbarEditButton()
+            it.toolbarButton.value = if (entries.isNotEmpty()) ButtonMode.EDIT else ButtonMode.NONE
         }
     }
 
@@ -210,7 +205,7 @@ abstract class DynamicListAdapter<T>(
         _entries.add(idx, item)
         notifyItemInserted(idx)
         listener?.onItemAdded(idx, item)
-        mainViewModel?.showToolbarEditButton()
+        mainViewModel?.toolbarButton?.value = ButtonMode.EDIT
     }
 
     @CallSuper
@@ -219,7 +214,7 @@ abstract class DynamicListAdapter<T>(
         notifyItemRemoved(idx)
         listener?.onItemRemoved(idx, item)
         if (entries.isEmpty())
-            mainViewModel?.hideToolbarEditButton()
+            mainViewModel?.toolbarButton?.value = ButtonMode.NONE
         return item
     }
 

app/src/main/java/org/fxboomk/fcitx5/android/ui/main/settings/ListFragment.kt
@@ -11,12 +11,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
 import org.fxboomk.fcitx5.android.core.Key
 import org.fxboomk.fcitx5.android.core.RawConfig
 import org.fxboomk.fcitx5.android.ui.common.BaseDynamicListUi
 import org.fxboomk.fcitx5.android.ui.common.DynamicListUi
 import org.fxboomk.fcitx5.android.ui.main.EditDeleteMenuProvider
 import org.fxboomk.fcitx5.android.ui.main.MainViewModel
+import org.fxboomk.fcitx5.android.ui.main.MainViewModel.ButtonMode
 import org.fxboomk.fcitx5.android.utils.config.ConfigDescriptor
 import org.fxboomk.fcitx5.android.utils.config.ConfigType
 import org.fxboomk.fcitx5.android.utils.lazyRoute
@@ -138,24 +140,27 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.toolbarButton.value =
+            if (ui.entries.isNotEmpty()) ButtonMode.EDIT else ButtonMode.NONE
         requireActivity().addMenuProvider(
-            EditDeleteMenuProvider(viewModel, requireActivity(), viewLifecycleOwner),
-            viewLifecycleOwner
+            EditDeleteMenuProvider(
+                buttonMode = viewModel.toolbarButton,
+                editButtonAction = { ui.enterMultiSelect(requireActivity().onBackPressedDispatcher) },
+                deleteButtonAction = { ui.deleteSelected(); ui.exitMultiSelect() },
+                menuHost = requireActivity(),
+                lifecycleOwner = viewLifecycleOwner,
+            ),
+            viewLifecycleOwner,
+            Lifecycle.State.STARTED
         )
     }
 
     override fun onStart() {
         super.onStart()
         viewModel.setToolbarTitle(descriptor.description ?: descriptor.name)
-        if (uiInitialized) {
-            viewModel.enableToolbarEditButton(ui.entries.isNotEmpty()) {
-                ui.enterMultiSelect(requireActivity().onBackPressedDispatcher)
-            }
-        }
     }
 
     override fun onStop() {
-        viewModel.disableToolbarEditButton()
         if (uiInitialized) {
             ui.exitMultiSelect()
         }

app/src/main/java/org/fxboomk/fcitx5/android/ui/main/settings/PunctuationEditorFragment.kt
@@ -6,6 +6,7 @@ package org.fxboomk.fcitx5.android.ui.main.settings
 
 import android.app.AlertDialog
 import android.view.View
+import androidx.lifecycle.Lifecycle
 import org.fxboomk.fcitx5.android.R
 import org.fxboomk.fcitx5.android.core.RawConfig
 import org.fxboomk.fcitx5.android.core.getPunctuationConfig
@@ -15,6 +16,7 @@ import org.fxboomk.fcitx5.android.data.punctuation.PunctuationMapEntry
 import org.fxboomk.fcitx5.android.ui.common.BaseDynamicListUi
 import org.fxboomk.fcitx5.android.ui.common.OnItemChangedListener
 import org.fxboomk.fcitx5.android.ui.main.EditDeleteMenuProvider
+import org.fxboomk.fcitx5.android.ui.main.MainViewModel.ButtonMode
 import org.fxboomk.fcitx5.android.utils.NaiveDustman
 import org.fxboomk.fcitx5.android.utils.lazyRoute
 import org.fxboomk.fcitx5.android.utils.materialTextInput
@@ -139,13 +141,19 @@ class PunctuationEditorFragment : ProgressFragment(), OnItemChangedListener<Punc
             }
         }
         resetDustman()
+        viewModel.toolbarButton.value =
+            if (ui.entries.isNotEmpty()) ButtonMode.EDIT else ButtonMode.NONE
         requireActivity().addMenuProvider(
-            EditDeleteMenuProvider(viewModel, requireActivity(), viewLifecycleOwner),
-            viewLifecycleOwner
+            EditDeleteMenuProvider(
+                buttonMode = viewModel.toolbarButton,
+                editButtonAction = { ui.enterMultiSelect(requireActivity().onBackPressedDispatcher) },
+                deleteButtonAction = { ui.deleteSelected(); ui.exitMultiSelect() },
+                menuHost = requireActivity(),
+                lifecycleOwner = viewLifecycleOwner,
+            ),
+            viewLifecycleOwner,
+            Lifecycle.State.STARTED
         )
-        viewModel.enableToolbarEditButton(initialEntries.isNotEmpty()) {
-            ui.enterMultiSelect(requireActivity().onBackPressedDispatcher)
-        }
         return ui.root
     }
 
@@ -172,16 +180,10 @@ class PunctuationEditorFragment : ProgressFragment(), OnItemChangedListener<Punc
     override fun onStart() {
         super.onStart()
         viewModel.setToolbarTitle(args.title)
-        if (isInitialized) {
-            viewModel.enableToolbarEditButton(ui.entries.isNotEmpty()) {
-                ui.enterMultiSelect(requireActivity().onBackPressedDispatcher)
-            }
-        }
     }
 
     override fun onStop() {
         saveConfig()
-        viewModel.disableToolbarEditButton()
         if (isInitialized) {
             ui.exitMultiSelect()
         }

app/src/main/java/org/fxboomk/fcitx5/android/ui/main/settings/QuickPhraseListFragment.kt
@@ -19,6 +19,7 @@ import androidx.appcompat.widget.PopupMenu
 import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -33,6 +34,7 @@ import org.fxboomk.fcitx5.android.ui.common.BaseDynamicListUi
 import org.fxboomk.fcitx5.android.ui.common.OnItemChangedListener
 import org.fxboomk.fcitx5.android.ui.main.EditDeleteMenuProvider
 import org.fxboomk.fcitx5.android.ui.main.MainViewModel
+import org.fxboomk.fcitx5.android.ui.main.MainViewModel.ButtonMode
 import org.fxboomk.fcitx5.android.utils.NaiveDustman
 import org.fxboomk.fcitx5.android.utils.importErrorDialog
 import org.fxboomk.fcitx5.android.utils.item
@@ -300,9 +302,18 @@ class QuickPhraseListFragment : Fragment(), OnItemChangedListener<QuickPhrase> {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.toolbarButton.value =
+            if (ui.entries.isNotEmpty()) ButtonMode.EDIT else ButtonMode.NONE
         requireActivity().addMenuProvider(
-            EditDeleteMenuProvider(viewModel, requireActivity(), viewLifecycleOwner),
-            viewLifecycleOwner
+            EditDeleteMenuProvider(
+                buttonMode = viewModel.toolbarButton,
+                editButtonAction = { ui.enterMultiSelect(requireActivity().onBackPressedDispatcher) },
+                deleteButtonAction = { ui.deleteSelected(); ui.exitMultiSelect() },
+                menuHost = requireActivity(),
+                lifecycleOwner = viewLifecycleOwner,
+            ),
+            viewLifecycleOwner,
+            Lifecycle.State.STARTED
         )
     }
 
@@ -323,18 +334,8 @@ class QuickPhraseListFragment : Fragment(), OnItemChangedListener<QuickPhrase> {
         dustman.addOrUpdate(new.name, new.isEnabled)
     }
 
-    override fun onStart() {
-        super.onStart()
-        if (uiInitialized) {
-            viewModel.enableToolbarEditButton(ui.entries.isNotEmpty()) {
-                ui.enterMultiSelect(requireActivity().onBackPressedDispatcher)
-            }
-        }
-    }
-
     override fun onStop() {
         reloadQuickPhrase()
-        viewModel.disableToolbarEditButton()
         if (uiInitialized) {
             ui.exitMultiSelect()
         }

app/src/main/java/org/fxboomk/fcitx5/android/ui/main/settings/im/InputMethodListFragment.kt
@@ -6,6 +6,7 @@ package org.fxboomk.fcitx5.android.ui.main.settings.im
 
 import android.os.Build
 import android.view.View
+import androidx.lifecycle.Lifecycle
 import org.fxboomk.fcitx5.android.core.InputMethodEntry
 import org.fxboomk.fcitx5.android.core.SubtypeManager
 import org.fxboomk.fcitx5.android.daemon.launchOnReady
@@ -13,6 +14,7 @@ import org.fxboomk.fcitx5.android.ui.common.BaseDynamicListUi
 import org.fxboomk.fcitx5.android.ui.common.DynamicListUi
 import org.fxboomk.fcitx5.android.ui.common.OnItemChangedListener
 import org.fxboomk.fcitx5.android.ui.main.EditDeleteMenuProvider
+import org.fxboomk.fcitx5.android.ui.main.MainViewModel.ButtonMode
 import org.fxboomk.fcitx5.android.ui.main.settings.ProgressFragment
 import org.fxboomk.fcitx5.android.ui.main.settings.SettingsRoute
 import org.fxboomk.fcitx5.android.utils.navigateWithAnim
@@ -52,30 +54,26 @@ class InputMethodListFragment : ProgressFragment(), OnItemChangedListener<InputM
         )
         ui.addOnItemChangedListener(this@InputMethodListFragment)
         ui.setViewModel(viewModel)
+        viewModel.toolbarButton.value =
+            if (ui.entries.isNotEmpty()) ButtonMode.EDIT else ButtonMode.NONE
         requireActivity().addMenuProvider(
-            EditDeleteMenuProvider(viewModel, requireActivity(), viewLifecycleOwner),
-            viewLifecycleOwner
+            EditDeleteMenuProvider(
+                buttonMode = viewModel.toolbarButton,
+                editButtonAction = { ui.enterMultiSelect(requireActivity().onBackPressedDispatcher) },
+                deleteButtonAction = { ui.deleteSelected(); ui.exitMultiSelect() },
+                menuHost = requireActivity(),
+                lifecycleOwner = viewLifecycleOwner,
+            ),
+            viewLifecycleOwner,
+            Lifecycle.State.STARTED
         )
-        viewModel.enableToolbarEditButton(initialEnabled.isNotEmpty()) {
-            ui.enterMultiSelect(requireActivity().onBackPressedDispatcher)
-        }
         return ui.root
-    }
-
-    override fun onStart() {
-        super.onStart()
-        if (::ui.isInitialized) {
-            viewModel.enableToolbarEditButton(ui.entries.isNotEmpty()) {
-                ui.enterMultiSelect(requireActivity().onBackPressedDispatcher)
-            }
-        }
     }
 
     override fun onStop() {
         if (::ui.isInitialized) {
             ui.exitMultiSelect()
         }
-        viewModel.disableToolbarEditButton()
         super.onStop()
     }
 